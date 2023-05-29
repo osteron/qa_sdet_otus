60a424db6d586423d9056d7a8981a65f53e2a11d
osteron <dimaslepyan@gmail.com>
2023-05-29 23:59:52 +0300

    refactor

--- api_testing/task_2/test_brewery_db.py
+++ api_testing/task_2/test_brewery_db.py
@@ -10,7 +10,7 @@ class TestPositive:
     @pytest.mark.parametrize('params, number',
                              [('', 50), ('per_page=1', 1), ('per_page=5', 5), ('per_page=0', 0)],
                              ids=['full list = 50', '1 per page', '5 per page', 'zero'])
-    def test_get_list_breweries(self, params, number):
+    def test_get_list_breweries(self, params: str, number: int) -> None:
         """
         Запрос на получение списка пивоваренных заводов.
         В параметре указывается количество отображаемых заводов.
@@ -28,15 +28,21 @@ class TestPositive:
         'city is Norman mentioned 3 times',
         'city is Austin and per page is 15'
     ])
-    def test_get_list_breweries_by_city(self, params, number):
+    def test_get_list_breweries_by_city(self, params: str, number: int) -> None:
+        """
+        Запрос на получение списка пивоваренных заводов с выборкой по городам и опционально с выбором количества
+        запрашиваемого количества заводов.
+        В параметре указывается город и опционально количество отображаемых заводов.
+        """
         response = requests.get(BASE_URL, params=params).json()
         assert len(response) == number
 
     @pytest.mark.smoke
-    @pytest.mark.parametrize('id, number', [('b54b16e1-ac3b-4bff-a11f-f7ae9ddc27e0', 16)], ids=['some brewery'])
-    def test_get_single_brewery(self, id, number):
-        response = requests.get(BASE_URL + id).json()
+    @pytest.mark.parametrize('id_num, number', [('b54b16e1-ac3b-4bff-a11f-f7ae9ddc27e0', 16)], ids=['some brewery'])
+    def test_get_single_brewery(self, id_num: str, number: int) -> None:
+        """
+        Запрос на получение информации о пивоваренном заводе по его id.
+        """
+        response = requests.get(BASE_URL + id_num).json()
         assert len(response) == number
-        assert response['id'] == id
-
-
+        assert response['id'] == id_num
