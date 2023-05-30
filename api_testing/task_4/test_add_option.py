c257343b01cfb2291e729210edae833d0fa1f413
osteron <dimaslepyan@gmail.com>
2023-05-30 23:56:07 +0300

    added files for a task 4

---
+++ api_testing/task_4/test_add_option.py
@@ -0,0 +1,8 @@
+import pytest
+import requests
+
+
+@pytest.mark.smoke
+def test_get_valid_status_code(base_url, status_code):
+    response = requests.get(url=base_url)
+    assert str(response.status_code) == status_code, f'The status code is expected {status_code}'
