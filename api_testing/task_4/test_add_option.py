6ecd1d03f6aa5200d520bb12a8ab73074c2ba844
osteron <dimaslepyan@gmail.com>
2023-05-30 23:57:59 +0300

    refactor file task 4

--- api_testing/task_4/test_add_option.py
+++ api_testing/task_4/test_add_option.py
@@ -3,6 +3,6 @@ import requests
 
 
 @pytest.mark.smoke
-def test_get_valid_status_code(base_url, status_code):
+def test_get_valid_status_code(base_url: str, status_code: str) -> None:
     response = requests.get(url=base_url)
     assert str(response.status_code) == status_code, f'The status code is expected {status_code}'
