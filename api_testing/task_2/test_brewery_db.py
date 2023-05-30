ed29c59b47e5324ebdf44e962753dbf2dff1d5d8
osteron <dimaslepyan@gmail.com>
2023-05-30 21:14:01 +0300

    added tests for task 2

--- api_testing/task_2/test_brewery_db.py
+++ api_testing/task_2/test_brewery_db.py
@@ -42,7 +42,34 @@ class TestPositive:
     def test_get_single_brewery(self, id_num: str, number: int) -> None:
         """
         Запрос на получение информации о пивоваренном заводе по его id.
+        Ответ содержит 16 значений.
         """
         response = requests.get(BASE_URL + id_num).json()
         assert len(response) == number
         assert response['id'] == id_num
+
+    @pytest.mark.smoke
+    @pytest.mark.parametrize('params, number', [('size=3', 3), ('size=10', 10), ('', 1), ('size=50', 50)],
+                             ids=['3 breweries', '10 breweries', '1 brewery - query without params', '50 is max'])
+    def test_get_random_brewery_with_size(self, params: str, number: int) -> None:
+        """
+        Запрос на получение информации о случайных пивоваренных заводах.
+        В параметре указывается опционально количество отображаемых заводов.
+        """
+        response = requests.get(BASE_URL + 'random', params=params).json()
+        assert len(response) == number
+
+
+class TestNegative:
+
+    @pytest.mark.skip(reason='dont fix bug')
+    @pytest.mark.regress
+    @pytest.mark.parametrize('params', ['-1', 'one', '!@##%#^%^@(*%&%@#_!'],
+                             ids=['negative number', 'string', 'some symbols'])
+    def test_error_get_list_brewery(self, params):
+        """
+        Отображение ошибки при невалидном значении параметра во время запроса списка всех
+        пивоваренных заводов.
+        """
+        response = requests.get(BASE_URL, params=params).json()
+        assert 'Couldn\'t find Brewery' in response['message']
