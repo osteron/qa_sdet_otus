fb6eec9a0ccfc72e05c79361654b205821ee277d
osteron <dimaslepyan@gmail.com>
2023-05-31 20:40:40 +0300

    refactor files for a task 1

--- api_testing/task_1/test_dog_api.py
+++ api_testing/task_1/test_dog_api.py
@@ -1,22 +1,37 @@
 import pytest
 import requests
 
+from .urls import Urls
+
 ERROR_SUCCESS_STATUS = 'Статус должен быть success'
+ERROR_STATUS_CODE_200 = 'Статус код должен быть 200'
+
+
+def get_status_code(url: str):
+    return requests.get(url).status_code
+
+
+def json_return(url: str):
+    return requests.get(url).json()
 
 
 class TestPositive:
 
     @pytest.mark.smoke
     @pytest.mark.parametrize('extension, status', [('.jpg', 'success')], ids=['extension .jpg and status "success"'])
-    def test_get_random_dog_image(self, base_url, random_image_url, extension, status):
+    def test_get_random_dog_image(self, extension, status):
         """
-        Ответ должен содержать:
-        'message': 'путь до фотографии с расширением .jpg'
-        'status': 'success'
+        Тест проверяет запрос на получение случайного изображения собаки.
+        Шаги проверки:
+        1. Проверка статус кода 200
+        2. Проверка наличия расширения файла в ответе.
+        3. Проверка статуса в ответе.
         """
-        response = requests.get(base_url + random_image_url).json()
-        assert extension in response['message']
-        assert response['status'] == status
+        url = Urls.RANDOM_IMAGE_URL
+        assert get_status_code(url) == 200, ERROR_STATUS_CODE_200
+        json_response = json_return(url)
+        assert extension in json_response['message'], f'Расширение файла должно быть {extension}'
+        assert status in json_response['status'], ERROR_SUCCESS_STATUS
 
     @pytest.mark.smoke
     @pytest.mark.parametrize('number, status', [
@@ -30,80 +45,82 @@ class TestPositive:
         'end of range minus one',
         'end of range'
     ])
-    def test_get_multiply_random_image(self, base_url, random_image_url, number, status):
+    def test_get_multiply_random_image(self, number, status):
         """
+        Тест проверяет запрос на получение нескольких случайных фотографий собак.
         Граничные значения для запроса от 1 до 50 (включительно).
-        Ответ должен содержать:
-        'message': ['путь до фотографии с расширением .jpg'] - список из N фотографий
-        'status': 'success'
+        Шаги проверки:
+        1. Проверка статус кода 200.
+        2. Проверка количества возвращаемых фотографий собак.
+        3. Проверка статуса в ответе.
         """
-        response = requests.get(base_url + random_image_url + number).json()
-        assert len(response['message']) == int(number), f'Возвращаемое количество должно быть равно {number}'
-        assert response['status'] == status, ERROR_SUCCESS_STATUS
+        url = f'{Urls.RANDOM_IMAGE_URL}{number}'
+        assert get_status_code(url) == 200, ERROR_STATUS_CODE_200
+        json_response = json_return(url)
+        assert len(json_response['message']) == int(number), f'Возвращаемое количество должно быть равно {number}'
+        assert status in json_response['status'], ERROR_SUCCESS_STATUS
 
     @pytest.mark.smoke
     @pytest.mark.parametrize('number, status', [(808, 'success')], ids=['count of images'])
-    def test_get_images_by_breed(self, base_url, by_breed_url, number, status):
+    def test_get_images_by_breed(self, number, status):
         """
-        Запрос на получение всех фотографий собак породы hound.
-        Ответ должен содержать:
-        'message': ['путь до фотографии с расширением .jpg'] - список из 808 фотографий
+        Тест проверяет запрос на получение всех фотографий собак породы hound.
+        Шаги проверки:
+        1. Проверка статус кода 200.
+        2. Проверка количества возвращаемых фотографий собак.
+        3. Проверка статуса в ответе.
         """
-        response = requests.get(base_url + by_breed_url).json()
-        assert len(response['message']) == number, f'Возвращаемое количество должно быть равно {number}'
-        assert response['status'] == status, ERROR_SUCCESS_STATUS
+        url = Urls.BY_BREED_URL
+        assert get_status_code(url) == 200, ERROR_STATUS_CODE_200
+        json_response = json_return(url)
+        assert len(json_response['message']) == number, f'Возвращаемое количество должно быть равно {number}'
+        assert status in json_response['status'], ERROR_SUCCESS_STATUS
 
     @pytest.mark.smoke
     @pytest.mark.parametrize('number, status', [(98, 'success')], ids=['count of breeds'])
-    def test_get_list_all_breeds(self, base_url, list_all_breeds_url, number, status):
+    def test_get_list_all_breeds(self, number, status):
         """
-        Запрос на получение списка всех пород собак.
-        Ответ должен содержать:
-        'message': {'порода':[]} - количество 98.
-        'status': 'success'
+        Тест проверяет запрос на получение списка всех пород собак.
+        Шаги проверки:
+        1. Проверка статус кода 200.
+        2. Проверка количества возвращаемых фотографий собак.
+        3. Проверка статуса в ответе.
         """
-        response = requests.get(base_url + list_all_breeds_url).json()
-        assert len(response['message']) == number, f'Возвращаемое количество должно быть равно {number}'
-        assert response['status'] == status, ERROR_SUCCESS_STATUS
+        url = Urls.LIST_ALL_BREEDS_URL
+        assert get_status_code(url) == 200, ERROR_STATUS_CODE_200
+        json_response = json_return(url)
+        assert len(json_response['message']) == number, f'Возвращаемое количество должно быть равно {number}'
+        assert status in json_response['status'], ERROR_SUCCESS_STATUS
 
     @pytest.mark.smoke
     @pytest.mark.parametrize('extension, status', [('.jpg', 'success')], ids=['extension .jpg and status "success"'])
-    def test_get_random_image_from_breed_collection(self, base_url, random_image_from_breed_url, extension, status):
+    def test_get_random_image_from_breed_collection(self, extension, status):
         """
-        Запрос на получение случайной фотографии собаки породы hound
-        Ответ должен содержать:
-        'message': 'путь до фотографии с расширением .jpg'
-        'status': 'success'
+        Тест проверяет запрос на получение случайной фотографии собаки породы hound
+        Шаги проверки:
+        1. Проверка статус кода 200.
+        2. Проверка наличия расширения файла в ответе.
+        3. Проверка статуса в ответе.
         """
-        response = requests.get(base_url + random_image_from_breed_url).json()
-        assert extension in response['message']
-        assert status in response['status']
+        url = Urls.RANDOM_IMAGE_FROM_BREED_URL
+        assert get_status_code(url) == 200, ERROR_STATUS_CODE_200
+        json_response = json_return(url)
+        assert extension in json_response['message'], f'Расширение файла должно быть {extension}'
+        assert status in json_response['status'], ERROR_SUCCESS_STATUS
 
     @pytest.mark.smoke
     @pytest.mark.parametrize('number, status', [(1, 'success'), (2, 'success'), (3, 'success')],
                              ids=['one image', 'two images', 'three images'])
-    def test_get_multiple_images_from_breed_collection(self, base_url, multiple_images_from_breed_url, number, status):
+    def test_get_multiple_images_from_breed_collection(self, number, status):
         """
-        Запрос на получение нескольких фотографий собак с породой hound.
-        Ответ должен содержать:
-        'message': ['путь до фотографии с расширением .jpg'] - N количество фотографий
-        'status': 'success'
+        Тест проверяет запрос на получение нескольких фотографий собак с породой hound.
+        Шаги проверки:
+        1. Проверка статус кода 200.
+        2. Проверка количества возвращаемых фотографий собак.
+        3. Проверка наличия статуса в ответе.
         """
-        response = requests.get(base_url + multiple_images_from_breed_url + str(number)).json()
-        assert len(response['message']) == number
-        assert status in response['status']
-
-
-class TestNegative:
-
-    @pytest.mark.skip(reason='not fix problem with out of range')
-    @pytest.mark.regress
-    @pytest.mark.parametrize('number', ['-1', '0', '51'], ids=['negative number', 'zero', 'out of range'])
-    def test_multiply_image_out_of_range(self, base_url, random_image_url, number):
-        """
-        Нарушение граничных значений запроса.
-        Ответ не должен содержать:
-        'status': 'success'
-        """
-        response = requests.get(base_url + random_image_url + number).json()
-        assert response['status'] != 'success', 'Статус не должен быть success'
+        url = f'{Urls.MULTIPLE_IMAGES_FROM_BREED_URL}{number}'
+        assert get_status_code(url) == 200, ERROR_STATUS_CODE_200
+        json_response = json_return(url)
+        assert len(json_response['message']) == number, f'Возвращаемое количество должно быть равно {number}'
+        assert status in json_response['status'], ERROR_SUCCESS_STATUS
